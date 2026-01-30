6bf6286ee056e1b99beb3c0eabae78c9aff87ae3
Awdhesh9211 <gaundawdhesh9211@gmail.com>
2026-01-30 10:18:37 +0530

    done loadbalancing with openfeign @LoadBalabce not required for openfign

--- consumer/src/main/java/com/learn/consumer/feignclient/ProviderFeignClient.java
+++ consumer/src/main/java/com/learn/consumer/feignclient/ProviderFeignClient.java
@@ -4,7 +4,9 @@ package com.learn.consumer.feignclient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name = "provider-service" , url = "http://localhost:8082")
+//@FeignClient(name = "provider-service" , url = "http://localhost:8082")
+
+@FeignClient(name = "provider-service" , url = "http://provider")
 public interface ProviderFeignClient {
 
 
