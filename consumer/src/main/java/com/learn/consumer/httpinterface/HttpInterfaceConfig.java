67a1c6f5b6d1a31fca78d3f487fd217b3a9f67a0
Awdhesh9211 <gaundawdhesh9211@gmail.com>
2026-01-30 12:28:07 +0530

    done http interface discovery eureka server

--- consumer/src/main/java/com/learn/consumer/httpinterface/HttpInterfaceConfig.java
+++ consumer/src/main/java/com/learn/consumer/httpinterface/HttpInterfaceConfig.java
@@ -1,23 +1,30 @@
 package com.learn.consumer.httpinterface;
 
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.client.support.RestTemplateAdapter;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.support.WebClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+import org.springframework.web.util.DefaultUriBuilderFactory;
 
 @Configuration
 public class HttpInterfaceConfig {
 
+//    @Bean
+//    @LoadBalanced
+//    public RestTemplate restTemplate(){
+//        return  new RestTemplate();
+//    }
+
 
     @Bean
-    public ProviderHttpInterface  webClientHttpInterface(){
-        WebClient webClient=WebClient
-                .builder()
-                .baseUrl("http://localhost:8082")
-                .build();
-        WebClientAdapter adapter=WebClientAdapter.create(webClient);
+    public ProviderHttpInterface  webClientHttpInterface(RestTemplate restTemplate){
+        restTemplate.setUriTemplateHandler(new DefaultUriBuilderFactory("http://provider"));
+        RestTemplateAdapter adapter=RestTemplateAdapter.create(restTemplate);
         HttpServiceProxyFactory factory=HttpServiceProxyFactory.builderFor(adapter).build();
 
         ProviderHttpInterface service=factory.createClient(ProviderHttpInterface.class);
